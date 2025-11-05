e069e6a1ac6e9c61f383717c26e0c7be23d2ced4
BBGoat <bbgoat@vip.qq.com>
2025-11-05 17:41:19 +0800

    将模组拆分为客户端和服务端 2个版本，此为服务端部分

--- modinfo.lua
+++ modinfo.lua
@@ -1,21 +1,13 @@
 ---@diagnostic disable: lowercase-global
-name = "强制纠正IP端口"
-version = "0.1.1"
+name = "强制纠正IP端口(服务端)"
+version = "0.2"
 description = [[
-设置本模组需订阅【便捷配置】或【文本模组配置】！
-本模组针对无法设置对等端口的服务器/同时拥有多个公网IP的服务器
-举例：你的服务器拥有2个公网IP，第一个是1.1.1.1，第二个是2.2.2.2
-你给1.1.1.1设置了端口转发 外网端口20999→本地端口10999 外网端口20998→本地端口10998
-但你上网优先使用外网IP 2.2.2.2
-这时候你给科雷上报的服务器信息就是： 服务器IP 2.2.2.2 地上世界端口10999 地下世界端口10998
-但客户端连接你的服务器需要使用 1.1.1.1 地上世界端口20999 地下世界端口 20998
-这时候就需要使用本模组来纠正IP和端口了！
-以上面的情况为例，你需要设置
-原IP-1 为 2.2.2.2 纠正IP为 1.1.1.1
-原端口-1 为 10999 纠正端口-1 为 20999
-原端口-2 为 10998 纠正端口-2 为 20998
-接着让客户端使用直连代码c_connect("1.1.1.1",20999)进入服务器，进服后本MOD就能为你工作了，
-而不是在玩家穿越世界时变为使用你上报给科雷的错误IP和端口，导致掉线或变为P2P连接
+设置本模组需订阅【配置扩展】！
+本模组分为客户端部分和服务器部分
+此为服务器部分，用于下发纠正后的IP端口
+客户端部分用于接收并应用服务器下发的IP端口
+
+玩家进入了开启本模组的服务器后会自动订阅并开启客户端部分的模组
 ]]
 author = "冰冰羊"
 api_version = 10
@@ -35,22 +27,22 @@ end
 configuration_options =
 {
     {
-        name = "DEBUG_print",
-        label = "打印纠正日志",
-        hover = "在客户端日志中打印纠正前后的IP和端口",
-        options =   {
-            {description = "开启", data = true},
-            {description = "关闭", data = false},
-        },
-        default = true
-    },
-    SkipSpace(),
+		name = "master_port",
+		label = "主世界服务器端口",
+		hover = "请填写主世界端口 server_port 例如：10999。不填的效果等于填10999",
+		options =	{
+						{description = "请开启配置扩展Mod", data = ""},
+					},
+		default = "",
+        is_text_config = true
+	},
+	SkipSpace(),
     {
 		name = "original_ip_1",
 		label = "原IP-1",
 		hover = "请填写服务器1的原始IP\n如：157.148.69.186\n如果想匹配任意内容，请填写0.0.0.0 这将导致客户端连接所有世界时都使用 纠正IP-1 同时其它2个IP设置将失效",
 		options =	{
-						{description = "请订阅便携配置Mod后进行设置", data = ""},
+						{description = "请开启配置扩展Mod", data = ""},
 					},
 		default = "",
         is_text_config = true
@@ -60,7 +52,7 @@ configuration_options =
 		label = "纠正IP-1",
 		hover = "请填写服务器1的纠正IP\n当客户端连接到“原IP-1”的服务器时，会强制纠正IP为纠正IP-1",
 		options =	{
-						{description = "请订阅便携配置Mod后进行设置", data = ""},
+						{description = "请开启配置扩展Mod", data = ""},
 					},
 		default = "",
         is_text_config = true
@@ -71,7 +63,7 @@ configuration_options =
 		label = "原IP-2",
 		hover = "请填写服务器2的原始IP",
 		options =	{
-						{description = "请订阅便携配置Mod后进行设置", data = ""},
+						{description = "请开启配置扩展Mod", data = ""},
 					},
 		default = "",
         is_text_config = true
@@ -81,7 +73,7 @@ configuration_options =
 		label = "纠正IP-2",
 		hover = "请填写服务器2的纠正IP\n当客户端连接到“原IP-2”的服务器时，会强制纠正IP为纠正IP-2",
 		options =	{
-						{description = "请订阅便携配置Mod后进行设置", data = ""},
+						{description = "请开启配置扩展Mod", data = ""},
 					},
 		default = "",
         is_text_config = true
@@ -92,7 +84,7 @@ configuration_options =
 		label = "原IP-3",
 		hover = "请填写服务器3的原始IP",
 		options =	{
-						{description = "请订阅便携配置Mod后进行设置", data = ""},
+						{description = "请开启配置扩展Mod", data = ""},
 					},
 		default = "",
         is_text_config = true
@@ -102,7 +94,7 @@ configuration_options =
 		label = "纠正IP-3",
 		hover = "请填写服务器3的纠正IP\n当客户端连接到“原IP-3”的服务器时，会强制纠正IP为纠正IP-3",
 		options =	{
-						{description = "请订阅便携配置Mod后进行设置", data = ""},
+						{description = "请开启配置扩展Mod", data = ""},
 					},
 		default = "",
         is_text_config = true
@@ -113,7 +105,7 @@ configuration_options =
 		label = "原端口-1",
 		hover = "请填写世界1的原始server_port",
 		options =	{
-						{description = "请订阅便携配置Mod后进行设置", data = ""},
+						{description = "请开启配置扩展Mod", data = ""},
 					},
 		default = "",
         is_text_config = true
@@ -123,7 +115,7 @@ configuration_options =
 		label = "纠正端口-1",
 		hover = "请填写“原端口-1”的纠正端口\n当客户端连接到“原端口-1”的世界时，会强制纠正端口为“纠正端口-1”",
 		options =	{
-						{description = "请订阅便携配置Mod后进行设置", data = ""},
+						{description = "请开启配置扩展Mod", data = ""},
 					},
 		default = "",
         is_text_config = true
@@ -134,7 +126,7 @@ configuration_options =
 		label = "原端口-2",
 		hover = "请填写世界2的原始server_port",
 		options =	{
-						{description = "请订阅便携配置Mod后进行设置", data = ""},
+						{description = "请开启配置扩展Mod", data = ""},
 					},
 		default = "",
         is_text_config = true
@@ -144,7 +136,7 @@ configuration_options =
 		label = "纠正端口-2",
 		hover = "请填写“原端口-2”的纠正端口\n当客户端连接到“原端口-2”的世界时，会强制纠正端口为“纠正端口-2”",
 		options =	{
-						{description = "请订阅便携配置Mod后进行设置", data = ""},
+						{description = "请开启配置扩展Mod", data = ""},
 					},
 		default = "",
         is_text_config = true
@@ -155,7 +147,7 @@ configuration_options =
 		label = "原端口-3",
 		hover = "请填写世界3的原始server_port",
 		options =	{
-						{description = "请订阅便携配置Mod后进行设置", data = ""},
+						{description = "请开启配置扩展Mod", data = ""},
 					},
 		default = "",
         is_text_config = true
@@ -165,7 +157,7 @@ configuration_options =
 		label = "纠正端口-3",
 		hover = "请填写“原端口-3”的纠正端口\n当客户端连接到“原端口-3”的世界时，会强制纠正端口为“纠正端口-3”",
 		options =	{
-						{description = "请订阅便携配置Mod后进行设置", data = ""},
+						{description = "请开启配置扩展Mod", data = ""},
 					},
 		default = "",
         is_text_config = true
@@ -176,7 +168,7 @@ configuration_options =
 		label = "原端口-4",
 		hover = "请填写世界4的原始server_port",
 		options =	{
-						{description = "请订阅便携配置Mod后进行设置", data = ""},
+						{description = "请开启配置扩展Mod", data = ""},
 					},
 		default = "",
         is_text_config = true
@@ -186,7 +178,7 @@ configuration_options =
 		label = "纠正端口-4",
 		hover = "请填写“原端口-4”的纠正端口\n当客户端连接到“原端口-4”的世界时，会强制纠正端口为“纠正端口-4”",
 		options =	{
-						{description = "请订阅便携配置Mod后进行设置", data = ""},
+						{description = "请开启配置扩展Mod", data = ""},
 					},
 		default = "",
         is_text_config = true
@@ -197,7 +189,7 @@ configuration_options =
 		label = "原端口-5",
 		hover = "请填写世界5的原始server_port",
 		options =	{
-						{description = "请订阅便携配置Mod后进行设置", data = ""},
+						{description = "请开启配置扩展Mod", data = ""},
 					},
 		default = "",
         is_text_config = true
@@ -207,7 +199,7 @@ configuration_options =
 		label = "纠正端口-5",
 		hover = "请填写“原端口-5”的纠正端口\n当客户端连接到“原端口-5”的世界时，会强制纠正端口为“纠正端口-5”",
 		options =	{
-						{description = "请订阅便携配置Mod后进行设置", data = ""},
+						{description = "请开启配置扩展Mod", data = ""},
 					},
 		default = "",
         is_text_config = true
@@ -218,7 +210,7 @@ configuration_options =
 		label = "原端口-6",
 		hover = "请填写世界6的原始server_port",
 		options =	{
-						{description = "请订阅便携配置Mod后进行设置", data = ""},
+						{description = "请开启配置扩展Mod", data = ""},
 					},
 		default = "",
         is_text_config = true
@@ -228,7 +220,7 @@ configuration_options =
 		label = "纠正端口-6",
 		hover = "请填写“原端口-6”的纠正端口\n当客户端连接到“原端口-6”的世界时，会强制纠正端口为“纠正端口-6”",
 		options =	{
-						{description = "请订阅便携配置Mod后进行设置", data = ""},
+						{description = "请开启配置扩展Mod", data = ""},
 					},
 		default = "",
         is_text_config = true
@@ -239,7 +231,7 @@ configuration_options =
 		label = "原端口-7",
 		hover = "请填写世界7的原始server_port",
 		options =	{
-						{description = "请订阅便携配置Mod后进行设置", data = ""},
+						{description = "请开启配置扩展Mod", data = ""},
 					},
 		default = "",
         is_text_config = true
@@ -249,7 +241,7 @@ configuration_options =
 		label = "纠正端口-7",
 		hover = "请填写“原端口-7”的纠正端口\n当客户端连接到“原端口-7”的世界时，会强制纠正端口为“纠正端口-7”",
 		options =	{
-						{description = "请订阅便携配置Mod后进行设置", data = ""},
+						{description = "请开启配置扩展Mod", data = ""},
 					},
 		default = "",
         is_text_config = true
@@ -260,7 +252,7 @@ configuration_options =
 		label = "原端口-8",
 		hover = "请填写世界8的原始server_port",
 		options =	{
-						{description = "请订阅便携配置Mod后进行设置", data = ""},
+						{description = "请开启配置扩展Mod", data = ""},
 					},
 		default = "",
         is_text_config = true
@@ -270,7 +262,7 @@ configuration_options =
 		label = "纠正端口-8",
 		hover = "请填写“原端口-8”的纠正端口\n当客户端连接到“原端口-8”的世界时，会强制纠正端口为“纠正端口-8”",
 		options =	{
-						{description = "请订阅便携配置Mod后进行设置", data = ""},
+						{description = "请开启配置扩展Mod", data = ""},
 					},
 		default = "",
         is_text_config = true
@@ -281,7 +273,7 @@ configuration_options =
 		label = "原端口-9",
 		hover = "请填写世界9的原始server_port",
 		options =	{
-						{description = "请订阅便携配置Mod后进行设置", data = ""},
+						{description = "请开启配置扩展Mod", data = ""},
 					},
 		default = "",
         is_text_config = true
@@ -291,7 +283,7 @@ configuration_options =
 		label = "纠正端口-9",
 		hover = "请填写“原端口-9”的纠正端口\n当客户端连接到“原端口-9”的世界时，会强制纠正端口为“纠正端口-9”",
 		options =	{
-						{description = "请订阅便携配置Mod后进行设置", data = ""},
+						{description = "请开启配置扩展Mod", data = ""},
 					},
 		default = "",
         is_text_config = true
--- modmain.lua
+++ modmain.lua
@@ -4,93 +4,26 @@ GLOBAL.setmetatable(env, {
     end
 })
 
-local original_ip_1 = GetModConfigData("original_ip_1")
-local original_ip_2 = GetModConfigData("original_ip_2")
-local original_ip_3 = GetModConfigData("original_ip_3")
 
-local fix_ip_1 = GetModConfigData("fix_ip_1")
-local fix_ip_2 = GetModConfigData("fix_ip_2")
-local fix_ip_3 = GetModConfigData("fix_ip_3")
+local client_modid = "dst-ip-fix" -- 客户端版模组ID
 
-local original_port_1 = GetModConfigData("original_port_1")
-local original_port_2 = GetModConfigData("original_port_2")
-local original_port_3 = GetModConfigData("original_port_3")
-local original_port_4 = GetModConfigData("original_port_4")
-local original_port_5 = GetModConfigData("original_port_5")
-local original_port_6 = GetModConfigData("original_port_6")
-local original_port_7 = GetModConfigData("original_port_7")
-local original_port_8 = GetModConfigData("original_port_8")
-local original_port_9 = GetModConfigData("original_port_9")
-
-local fix_port_1 = GetModConfigData("fix_port_1")
-local fix_port_2 = GetModConfigData("fix_port_2")
-local fix_port_3 = GetModConfigData("fix_port_3")
-local fix_port_4 = GetModConfigData("fix_port_4")
-local fix_port_5 = GetModConfigData("fix_port_5")
-local fix_port_6 = GetModConfigData("fix_port_6")
-local fix_port_7 = GetModConfigData("fix_port_7")
-local fix_port_8 = GetModConfigData("fix_port_8")
-local fix_port_9 = GetModConfigData("fix_port_9")
-
-local function DEBUG_print(...)
-    if GetModConfigData("DEBUG_print") then
-        print(...)
-    end
-end
-
--- 将字符串安全转换为数字
-local function tonumber_or_nil(x)
-    local n = tonumber(x)
-    return n
-end
-
--- 将原始端口与配置项比较，并替换为对应的纠正端口
-local function fix_port(port)
-    if port == tonumber_or_nil(original_port_1) then
-        return tonumber_or_nil(fix_port_1)
-    elseif port == tonumber_or_nil(original_port_2) then
-        return tonumber_or_nil(fix_port_2)
-    elseif port == tonumber_or_nil(original_port_3) then
-        return tonumber_or_nil(fix_port_3)
-    elseif port == tonumber_or_nil(original_port_4) then
-        return tonumber_or_nil(fix_port_4)
-    elseif port == tonumber_or_nil(original_port_5) then
-        return tonumber_or_nil(fix_port_5)
-    elseif port == tonumber_or_nil(original_port_6) then
-        return tonumber_or_nil(fix_port_6)
-    elseif port == tonumber_or_nil(original_port_7) then
-        return tonumber_or_nil(fix_port_7)
-    elseif port == tonumber_or_nil(original_port_8) then
-        return tonumber_or_nil(fix_port_8)
-    elseif port == tonumber_or_nil(original_port_9) then
-        return tonumber_or_nil(fix_port_9)
-    else
-        return port
-    end
-end
-
-if GLOBAL.NetworkProxy then
-    local oldStartClient = GLOBAL.NetworkProxy.StartClient
-    GLOBAL.NetworkProxy.StartClient = function(self, ip, port, id, password, netid, ...)
-        DEBUG_print("[强制纠正IP端口] 原始IP", ip, "原始端口", port)
-
-        if netid then
-            DEBUG_print("[强制纠正IP端口] 将netid", netid, "删除！")
-            netid = nil
+local function Auto_Enable_Mod()
+    TheGlobalInstance:DoTaskInTime(10, function() -- 等10秒检查模组是否下载完成
+        if KnownModIndex:GetModInfo(client_modid) then -- 下载完成，启用模组
+            KnownModIndex:Enable(client_modid)
+            KnownModIndex:Save()
+        else -- 未下载完成，再次尝试
+            Auto_Enable_Mod()
         end
-        if original_ip_1 == "0.0.0.0" then
-            ip = fix_ip_1
-        elseif ip == original_ip_1 then
-            ip = fix_ip_1
-        elseif ip == original_ip_2 then
-            ip = fix_ip_2
-        elseif ip == original_ip_3 then
-            ip = fix_ip_3
-        end
-
-        port = fix_port(port)
-
-        DEBUG_print("[强制纠正IP端口] 纠正后的IP", ip, "纠正后的端口", port)
-        return oldStartClient(self, ip, port, id, password, netid, ...)
-    end
+    end)
 end
+
+-- 游戏初始化后检测是否安装/启用客户端模组
+AddGamePostInit(function()
+    if not KnownModIndex:GetModInfo(client_modid) then
+        TheSim:SubscribeToMod(client_modid)
+        Auto_Enable_Mod()
+    elseif not KnownModIndex:IsModEnabledAny(client_modid) then
+        Auto_Enable_Mod()
+    end
+end)
