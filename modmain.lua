ed7129892ec1e4c27b238a818013fe20b16f0e88
BBGoat <bbgoat@vip.qq.com>
2026-03-02 00:53:04 +0800

    v0.1.8 已注释功能：从服务器大厅进服时现在尝试也会触发纠正机制了

--- modmain.lua
+++ modmain.lua
@@ -90,6 +90,13 @@ end
 -- local original_port_1, original_port_2, original_port_3, original_port_4, original_port_5, original_port_6, original_port_7, original_port_8, original_port_9
 -- local fix_port_1, fix_port_2, fix_port_3, fix_port_4, fix_port_5, fix_port_6, fix_port_7, fix_port_8, fix_port_9
 
+-- 读取上次记录的模组配置
+-- DEBUG_print("[强制纠正IP端口] 正在读取上次记录的配置")
+for k,v in pairs(RW_Data:LoadData()) do
+    -- if k ~= "" and v ~= "" then DEBUG_print(k,"=",v) end
+    env[k] = v
+end
+
 -- 将字符串安全转换为数字
 local function tonumber_or_nil(x)
     local n = tonumber(x)
@@ -154,23 +161,19 @@ end
 
 服务器没开纠正模组：
 玩家连接服务器 -> 不纠正IP端口 -> 连接上了服务器 -> 接收到模组设置 -> 服务器没开纠正模组 -> 进服开玩
+
+从大厅搜索服务器进：
+检查服务器的IP是否等于config.last_server_ip，等于就纠正后进
 ]]
 
--- 读取上次记录的模组配置
--- DEBUG_print("[强制纠正IP端口] 正在读取上次记录的配置")
-for k,v in pairs(RW_Data:LoadData()) do
-    -- if k ~= "" and v ~= "" then DEBUG_print(k,"=",v) end
-    env[k] = v
-end
-
-local current_original_ip
+local current_original_ip, current_original_port -- 当前服务器原始IP端口
 local current_target_ip, current_target_port, current_target_password -- 当前连接的房间IP、端口、密码(连接从世界时分配的是随机密码，会影响重连，所以用不上，缺点是可能需要玩家手动再输入一遍密码)
 local need_reconnect = false -- 是否需要纠正（重新连接服务器）
 local old_SetTempModConfigData = KnownModIndex.SetTempModConfigData
 KnownModIndex.SetTempModConfigData = function(self, temp_mods_config_data, ...) -- 进服/穿越世界时服务器会下发模组设置给客户端
     old_SetTempModConfigData(self, temp_mods_config_data, ...)
 
-    if not IsPrivateIP(current_target_ip) then
+    if current_target_ip and not IsPrivateIP(current_target_ip) then
         for modname, config_data in pairs(temp_mods_config_data) do
             if modname == server_modid then
                 config_data.have_server_mod = true
@@ -186,7 +189,7 @@ KnownModIndex.SetTempModConfigData = function(self, temp_mods_config_data, ...) 
                 end
 
                 -- 跑一遍纠正函数 看看是否需要纠正。如果IP是域名 说明已经纠正过了
-                if (current_target_ip and string.find(current_target_ip, "^%d+%.%d+%.%d+%.%d+$") and current_target_ip ~= fix_ip(current_target_ip)) or current_target_port ~= fix_port(current_target_port) then
+                if (string.find(current_target_ip, "^%d+%.%d+%.%d+%.%d+$") and current_target_ip ~= fix_ip(current_target_ip)) or current_target_port ~= fix_port(current_target_port) then
                     DEBUG_print("[强制纠正IP端口] 检测到当前需要纠正IP端口")
                     if current_target_ip ~= fix_ip(current_target_ip) then
                         DEBUG_print("当前IP", current_target_ip, "需要纠正为", fix_ip(current_target_ip))
@@ -223,35 +226,77 @@ end
 
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
+local function fix_ip_port(ip, port)
+    local fixed_ip, fixed_port, fixed -- 是否纠正过
+    local config = RW_Data:LoadData()
+    if config.have_server_mod or config.last_server_ip == ip then -- 如果服务器开启了纠正模组，或服务器IP和上次记录的IP一致(防止连都连不上, 无法判断是否开模组)，就使用纠正
+        DEBUG_print("[强制纠正IP端口] 原始IP", ip, "原始端口", port)
+
+        if not IsPrivateIP(ip) then -- 连接内网IP时不纠正
+            fixed_ip = fix_ip(ip)
+            fixed_port = fix_port(port)
+        end
+
+        DEBUG_print("[强制纠正IP端口] 纠正后的IP", ip, "纠正后的端口", port)
+        fixed = ip ~= fixed_ip or port ~= fixed_port
+    else
+        DEBUG_print("[强制纠正IP端口] 当前进入的服务器不需要纠正IP端口\n上个需要纠正IP端口的服务器IP：" .. tostring(config.last_server_ip) , "本次连接的服务器IP：", tostring(ip))
+    end
+
+    current_target_ip = fixed_ip
+    current_target_port = fixed_port
+    return fixed_ip or ip, fixed_port or port, fixed
+end
+
+local NeedHookTheNetJoinServerResponse = false
 if GLOBAL.NetworkProxy then
     local oldStartClient = GLOBAL.NetworkProxy.StartClient
     GLOBAL.NetworkProxy.StartClient = function(self, ip, port, id, password, netid, ...)
-        if not port or port == "" then port = 10999 end
         current_original_ip = ip
 
-        local config = RW_Data:LoadData()
-        if config.have_server_mod or config.last_server_ip == ip then -- 如果服务器开启了纠正模组，或服务器IP和上次记录的IP一致(防止连都连不上, 无法判断是否开模组)，就使用纠正
-            DEBUG_print("[强制纠正IP端口] 原始IP", ip, "原始端口", port)
-            RW_Data:SaveData(config)
+        if not port or port == "" then port = 10999 end
+        local fixed_ip, fixed_port, fixed = fix_ip_port(ip, port)
 
-            if netid then
-                DEBUG_print("[强制纠正IP端口] 将netid", netid, "删除！")
-                netid = nil
-            end
-
-            if not IsPrivateIP(ip) then -- 连接内网IP时不纠正
-                ip = fix_ip(ip)
-                port = fix_port(port)
-            end
-
-            DEBUG_print("[强制纠正IP端口] 纠正后的IP", ip, "纠正后的端口", port)
+        if fixed and netid then
+            DEBUG_print("[强制纠正IP端口] 将netid", netid, "删除！")
+            netid = nil
         end
 
-        current_target_ip = ip
-        current_target_port = port
-        return oldStartClient(self, ip, port, id, password, netid, ...)
+        return oldStartClient(self, fixed_ip, fixed_port, id, password, netid, ...)
+    end
+
+    -- 从大厅进最终会使用到这个方法，所以HOOK这个方法
+    local oldJoinServerResponse = NetworkProxy.JoinServerResponse
+    NetworkProxy.JoinServerResponse = function(self, ...)
+        if NeedHookTheNetJoinServerResponse then
+            DEBUG_print("[强制纠正IP端口] 当前需要纠正IP端口！")
+            c_connect(current_original_ip, current_original_port) -- 现在压力给到NetworkProxy.StartClient
+            NeedHookTheNetJoinServerResponse = false
+            return
+        end
+        return oldJoinServerResponse(self, ...)
     end
 end
+
+AddClassPostConstruct("screens/redux/serverlistingscreen", function(self)
+    local old_Join = self.Join
+    self.Join = function(warnedOffline, warnedLanguage, warnedPaused, ...)
+        local selected_index_actual = self.selected_index_actual -- 当前选中的服务器索引
+        local sever_info = TheNet:GetServerListingFromActualIndex(selected_index_actual)
+        current_original_ip = sever_info and sever_info.ip
+        current_original_port = sever_info and sever_info.port
+        if current_original_ip and current_original_port then
+            DEBUG_print("[强制纠正IP端口] 当前从服务器列表进入的服务器IP端口为", current_original_ip, current_original_port)
+
+            local config = RW_Data:LoadData()
+            if config.last_server_ip == current_original_ip then
+                NeedHookTheNetJoinServerResponse = true
+            end
+        end
+
+        return old_Join(self, warnedOffline, warnedLanguage, warnedPaused, ...)
+    end
+end)
 
 -- 回到主菜单时重置have_server_mod
 AddClassPostConstruct("screens/redux/mainscreen", function()
